f79b079b7c7b8f7023cb6730e182303d0a3eabed
FilipeTenedini <tenedinifilipe@gmail.com>
2024-05-04 10:56:19 -0300

    feat: start micro service feature (receiver)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('task_notification')
+  taskNotification(data: unknown) {
+    console.log('Receive message');
+    console.log(data);
   }
 }
