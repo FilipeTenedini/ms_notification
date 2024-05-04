a4bbc48020497646f046bbcc7bb05359c6594e3d
FilipeTenedini <tenedinifilipe@gmail.com>
2024-05-04 15:40:15 -0300

    feat: make micro service send email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+
+export type TaskUserEntity = {
+  id: string;
+  userId: string;
+  taskId: string;
+  createdAt: Date;
+  user?: {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    avatarUrl: string;
+    createdAt: string;
+  };
+  task?: {
+    id: string;
+    title: string;
+    description: string;
+    startAt: Date;
+    endAt: Date;
+    priority: 'HIGH' | 'MEDIUM' | 'LOW';
+    status: 'PENDING' | 'COMPLETED' | 'ONGOING';
+  };
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   @EventPattern('task_notification')
-  taskNotification(data: unknown) {
-    console.log('Receive message');
-    console.log(data);
+  async taskNotification(data: TaskUserEntity) {
+    await this.mailerService.sendMail({
+      to: data.user?.email,
+      from: 'taskmanager@taskmanager.net',
+      subject: 'Notificação de Tarefa',
+      text: `Olá ${data?.user?.name}! Você tem atividade que se inicia as ${data.task?.startAt} e finaliza as ${data.task?.endAt}`,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'brenna.stanton@ethereal.email',
+          pass: 'Tgg3PDcf28Mb22Cvrf',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
